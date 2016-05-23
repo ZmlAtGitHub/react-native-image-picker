e6d2495fafcd7a703466e3e8dffed78a3eff6d4b
LvBingru <lvbingru@gmail.com>
2016-05-23 12:18:55 +0800

    1.0.7

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
  * Created by lvbingru on 2015-12-29.
  */
 
+import {NativeModules} from 'react-native';
 
 import SystemImagePicker from './SystemImagePicker';
 import ImageAddBlock from './js/ImageAddBlock';
 import ImagePicker from './js/ImagePicker';
 
+const fixImageOrientation =  NativeModules.BBImageUtil && NativeModules.BBImageUtil.fixedOrientationOfImage;
+
 module.exports = {
   SystemImagePicker,
   ImageAddBlock,
   ImagePicker,
+  fixImageOrientation,
 };
